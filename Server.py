827b4cdf72f2a799f8a806fa38ff2eaae2d171ac
Mohammad Abu Alhalawe <mhalaweh2006@hotmail.com>
2020-02-05 13:43:22 +0200

    with edits

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
@@ -7,7 +7,31 @@ app = Flask(__name__)
 @app.route("/sanity")
 def foo():
     print("got request")
-    return json.dumps({'hello' : 'its me'})
+    return json.dumps({'hello': 'its me'})
+
+
+@app.route("/home")
+def bar():
+    print("got request")
+    return json.dumps("Home")
+
+
+@app.route('/users/<id>')
+def get_user(id):
+    return json.dumps(users[id])
+
+
+users = {
+    "123": {"name": "bubu"},
+    "124": {"name": "kuku"}
+}
+
+
+@app.route("/users")
+def get_users():
+    start = request.args.get("start")
+    users_with_b = [users[s] for s in users if users[s]["name"].startswith(start)]
+    return json.dumps(users_with_b)
 
 
 if __name__ == '__main__':
