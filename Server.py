abab3b48ed266bd62d2d6217ca2a1accef4d82cb
Mohammad Abu Alhalawe <mhalaweh2006@hotmail.com>
2020-02-05 15:32:55 +0200

    with exercise 1

--- Server.py
+++ Server.py
@@ -1,13 +1,34 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import json
+import requests
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='', static_folder='dist')
+log = []
+data = {
+    "8112": {
+        "title": "Name of the Wind",
+        "author": "Patrick Rothfuss"
+    },
+    "9121": {
+        "title": "The Catcher in the Rye",
+        "author": "J.D. Salinger"
+    },
+    "1081": {
+        "title": "The Giver",
+        "author": "Lois Lowry"
+    }
+}
+store = [
+    { "name": "table", "inventory": 3, "price": 800 },
+    { "name": "chair", "inventory": 16, "price": 120 },
+    { "name": "couch", "inventory": 1, "price": 1200 },
+    { "name": "picture frame", "inventory": 31, "price": 70 }
+]
 
 @app.route("/sanity")
 def foo():
     print("got request")
-    return json.dumps({'hello': 'its me'})
+    return json.dumps("Server is up and running smoothly")
 
 
 @app.route("/home")
@@ -21,18 +42,68 @@ def get_user(id):
     return json.dumps(users[id])
 
 
+@app.route('/life')
+def set_life():
+    return "42"
+
+
+@app.route('/landing/<username>')
+def greetuser(username):
+    return Response("Hi there {}".format(username))
+
+
 users = {
-    "123": {"name": "bubu"},
-    "124": {"name": "kuku"}
+    "tilda": "You've done a wonderful job",
+    "jeremy": "You need to improve your form, but good perseverance",
+    "Marta": "You're incredible"
 }
 
 
-@app.route("/users")
-def get_users():
-    start = request.args.get("start")
-    users_with_b = [users[s] for s in users if users[s]["name"].startswith(start)]
+@app.route("/user/<usersID>")
+def get_users(usersID):
+    start = request.args.get(usersID)
+    users_with_b = [users[s] for s in users if s == usersID]
     return json.dumps(users_with_b)
+
+
+@app.route('/optionalParameters')
+def query_params():
+    json_string = json.dumps(request.args)
+    return Response(json_string)
+
+
+@app.route('/details')
+def query_city():
+    json_string = json.dumps(request.args)
+    log.append(json_string)
+    return Response(json_string)
+
+
+@app.route('/print')
+def print_log():
+    return json.dumps(log)
+
+
+@app.route('/books')
+def get_book():
+    json_string = json.dumps(request.args)
+    return Response(json_string)
+
+
+@app.route('/<path:file_path>')
+def serve_static_file(file_path):
+    return app.send_static_file(file_path)
+
+
+@app.route('/priceCheck/<furname>')
+def price_check(furname):
+    json_string = request.args.get(furname)
+
+    return json.dumps()
 
 
 if __name__ == '__main__':
     app.run(port=3000)
+    books_url = 'http://localhost:3000/books/{}'.format(id)
+    res = requests.get(url=books_url)
+    print(res.json())
